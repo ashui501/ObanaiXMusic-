398b171e53805ffbe584fc68df34666076ff2eea
AKIRA ISHIKKI <113001130+ashui501@users.noreply.github.com>
2024-10-26 20:32:16 +0530

    Update config.py

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ HEROKU_API_KEY = getenv("HEROKU_API_KEY")
 
 UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
-    "https://github.com/ashui501/BADNAMXMUSIC",
+    "https://github.com/ashui501/ObanaiXMusic-",
 )
 UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
 GIT_TOKEN = getenv(
